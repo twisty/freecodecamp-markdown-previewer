d665eaee85ddf557339835c1cca0fb064b097099
Tim Brayshaw <tim@brayshaw.com>
2017-01-28 22:02:27 +0000

    Intial commit of functional app.

---
+++ README.md
@@ -0,0 +1,8 @@
+# FreeCodeCamp Markdown Editor
+
+The FreeCodeCamp challenge is to [Build a Markdown Previewer](https://www.freecodecamp.com/challenges/build-a-markdown-previewer).
+
+1. **User Story:** I can type GitHub-flavored Markdown into a text area.
+2. **User Story:** I can see a preview of the output of my markdown that is updated as I type.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import remark from 'remark';
+import reactRenderer from 'remark-react';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            markdown: props.markdown
+        };
+        this.editorTextarea = null;
+    }
+
+    componentDidMount() {
+        this.editorTextarea.focus();
+    }
+
+    handleChange = (e) => {
+        this.setState({markdown: e.target.value});
+    }
+
+    transformMarkdown = (markdown) => {
+        return remark().use(reactRenderer).process(this.state.markdown).contents;
+    }
+
+    render() {
+        return (
+            <div className="d-flex align-items-stretch h-100">
+                <div id="editor" className="w-50">
+                    <label htmlFor="editor-textarea" className="sr-only">Markdown</label>
+                    <textarea
+                        id="editor-textarea"
+                        placeholder="# Markdown"
+                        className="form-control markdown h-100"
+                        onChange={this.handleChange}
+                        value={this.state.markdown}
+                        ref={(node) => this.editorTextarea = node}
+                    />
+                </div>
+                <div id="preview" className="w-50 p-4">
+                    {this.transformMarkdown(this.state.markdown)}
+                </div>
+            </div>
+        );
+    }
+}
+
+App.defaultProps = {
+    markdown: '# Markdown Editor'
+}
+
+export default App;
